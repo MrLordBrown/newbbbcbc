250801665e79a0291ea62694e5dce7c6ce5d33d9
MrLordBrown <mrlordvondoombraun@gmail.com>
2021-12-27 05:45:14 -0800

    2 files modified

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-$(function() {
-  $('.wrapper').click(function() {
-      $('.wrapper').each(function() {
-         $(this).css('z-index', 0); 
-      });
-      	$(this).css('z-index', 10); 
-      	$(this).toggleClass('open');    
-  }) 
-})
+var wrapit = document.getElementsByClassName('wrapper');
+
+for (var i = 0; i < wrapit.length; i++) {
+  wrapit[i].addEventListener('click', wrappy);
+}
+function wrappy() {
+  var w = this.style.zindex;
+  if (w == 0) {
+    w = 10;
+  } else {
+    w = 0;
+  }
+  this.classList.toggle('open');
+}
